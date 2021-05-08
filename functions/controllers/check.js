dedefd1bed8c8049b81f6a01ecd9ffa780da0525
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-08 16:28:34 +0530

    fix

--- functions/controllers/check.js
+++ functions/controllers/check.js
@@ -56,70 +56,77 @@ var respons = [
 ];
 
 exports.check = async (reqq, resp) => {
-  await statusCheck.then(result => {
-    respons[0].status = result;
+
+  const statusCheck = new Promise((resolve, reject) => {
+
+    const https = require("https");
+  
+    const options = {
+      hostname: "fossnsbm.org",
+      path: "/",
+      method: "GET",
+    };
+  
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+      if (res.statusCode == 200) {
+        resolve("Active");
+      } else {
+        resolve("Inactive");
+      }
+    });
+  
+    req.on("error", (error) => {
+      console.error(error);
+      // respons[id].status = "Loading";
+    });
+  
+    req.end();
+  });
+  
+  const statusCheck2 = new Promise((resolve, reject) => {
+  
+    const https = require("https");
+  
+    const options = {
+      hostname: "forum.fossnsbm.org",
+      path: "/",
+      method: "GET",
+    };
+  
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+      if (res.statusCode == 200) {
+        resolve("Active");
+      } else {
+        resolve("Inactive");
+      }
+    });
+  
+    req.on("error", (error) => {
+      console.error(error);
+      // respons[id].status = "Loading";
+    });
+  
+    req.end();
+  });
+
+  statusCheck.then(result => {
+    // respons[0].status = result;
     // resp.send(respons);
     console.log(result);
   })
 
-  await statusCheck2.then(result => {
-    respons[1].status = result;
+  statusCheck2.then(result => {
+    // respons[1].status = result;
     console.log(result);
+  })
+
+  await Promise.all([statusCheck, statusCheck2]).then(result => {
+    respons[0].status = result[0];
+    respons[1].status = result[1];
   })
   resp.send(respons);
   // resp.send("Hello World");
 };
 
-const statusCheck = new Promise((resolve, reject) => {
-
-  const https = require("https");
-
-  const options = {
-    hostname: "fossnsbm.org",
-    path: "/",
-    method: "GET",
-  };
-
-  const req = https.request(options, (res) => {
-    console.log(`statusCode: ${res.statusCode}`);
-    if (res.statusCode == 200) {
-      resolve("Active");
-    } else {
-      resolve("Inactive");
-    }
-  });
-
-  req.on("error", (error) => {
-    console.error(error);
-    // respons[id].status = "Loading";
-  });
-
-  req.end();
-});
-
-const statusCheck2 = new Promise((resolve, reject) => {
-
-  const https = require("https");
-
-  const options = {
-    hostname: "forum.fossnsbm.org",
-    path: "/",
-    method: "GET",
-  };
-
-  const req = https.request(options, (res) => {
-    console.log(`statusCode: ${res.statusCode}`);
-    if (res.statusCode == 200) {
-      resolve("Active");
-    } else {
-      resolve("Inactive");
-    }
-  });
-
-  req.on("error", (error) => {
-    console.error(error);
-    // respons[id].status = "Loading";
-  });
-
-  req.end();
-});
