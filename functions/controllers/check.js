809e66abc86814bc4880b97801f83ecd5bdd35db
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-13 07:49:35 +0530

    fix

--- functions/controllers/check.js
+++ functions/controllers/check.js
@@ -55,76 +55,49 @@ var respons = [
   },
 ];
 
+const testUrl = [
+  "fossnsbm.org",
+  "forum.fossnsbm.org",
+]
+
 exports.check = async (reqq, resp) => {
 
-  const statusCheck = new Promise((resolve, reject) => {
+  const statusCheck = (url) => {
+    return new Promise((resolve, reject) => {
 
-    const https = require("https");
-  
-    const options = {
-      hostname: "fossnsbm.org",
-      path: "/",
-      method: "GET",
-    };
-  
-    const req = https.request(options, (res) => {
-      console.log(`statusCode: ${res.statusCode}`);
-      if (res.statusCode == 200) {
-        resolve("Active");
-      } else {
-        resolve("Inactive");
-      }
+      const https = require("https");
+    
+      const options = {
+        hostname: url,
+        path: "/",
+        method: "GET",
+      };
+    
+      const req = https.request(options, (res) => {
+        console.log(`statusCode: ${res.statusCode}`);
+        if (res.statusCode == 200) {
+          resolve("Active");
+        } else {
+          resolve("Inactive");
+        }
+      });
+    
+      req.on("error", (error) => {
+        console.error(error);
+        // respons[id].status = "Loading";
+      });
+    
+      req.end();
     });
+  }
   
-    req.on("error", (error) => {
-      console.error(error);
-      // respons[id].status = "Loading";
-    });
-  
-    req.end();
-  });
-  
-  const statusCheck2 = new Promise((resolve, reject) => {
-  
-    const https = require("https");
-  
-    const options = {
-      hostname: "forum.fossnsbm.org",
-      path: "/",
-      method: "GET",
-    };
-  
-    const req = https.request(options, (res) => {
-      console.log(`statusCode: ${res.statusCode}`);
-      if (res.statusCode == 200) {
-        resolve("Active");
-      } else {
-        resolve("Inactive");
-      }
-    });
-  
-    req.on("error", (error) => {
-      console.error(error);
-      // respons[id].status = "Loading";
-    });
-  
-    req.end();
-  });
 
-  statusCheck.then(result => {
-    // respons[0].status = result;
-    // resp.send(respons);
-    console.log(result);
-  })
-
-  statusCheck2.then(result => {
-    // respons[1].status = result;
-    console.log(result);
-  })
-
-  await Promise.all([statusCheck, statusCheck2]).then(result => {
+  await Promise.all(testUrl.map((value, key) => {
+    return statusCheck(value);
+  })).then(result => {
     respons[0].status = result[0];
     respons[1].status = result[1];
+    // console.log(result);
   })
   resp.json({statusData:respons});
   // resp.send("Hello World");
